73c0f78b0c06c752753924e3947e0d4d9595c2a4
AlexisADpm <alexisperez.1120@gmail.com>
2025-12-03 20:45:40 -0300

    diseno inicial para landing page de analisis de videojuegos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Main } from './components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Main],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'bmwsales_app';
+  title = 'Analisis Videojuegos';
 }
